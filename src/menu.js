d0a2b867f1fc26312d79a50661c2d980e5b6f3ff
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-06-15 12:45:28 -0500

    add about, contact and menu js files add styles to styles.css

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,12 @@
 const hero = document.createElement('div');
-const about = document.createElement('div');
 const menu = document.createElement('div');
-const faq = document.createElement('div');
 const contact = document.createElement('div');
 const content = document.getElementById('content');
+const menuBtn = document.querySelector('#menu');
 
-export function loadPage() {
+export function menuPage() {
     hero.classList.add('hero');
-    about.classList.add('about');
     menu.classList.add('menu');
-    faq.classList.add('faq');
     contact.classList.add('contact');
 
     hero.innerHTML = `
@@ -17,22 +14,6 @@ export function loadPage() {
                 <h1>Welcome to Our Restaurant</h1>
                 <p>Enjoy the best food in town!</p>
             </div>
-    `;
-
-    about.innerHTML = `
-    <div class="wrapper-col"> 
-    <div class="about-text">
-                <h2>Our Story</h2>
-                <p>Founded in 2000, our restaurant has been a staple in the community for over two decades. 
-                We pride ourselves on our commitment to quality and customer satisfaction.</p>
-                
-        </div>
-            
-        <div class="about-img">
-                <img src="https://images.unsplash.com/photo-1525610553991-2bede1a236e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Restaurant Image">
-        </div>
-    </div>
-        
     `;
 
     menu.innerHTML = `
@@ -100,20 +81,6 @@ export function loadPage() {
         
         `;
 
-    faq.innerHTML = `
-    <div class="wrapper"> 
-         <h2>Frequently Asked Questions</h2>
-            <div class="faq-item">
-                <h3>What are your hours of operation?</h3>
-                <p>We are open daily from 11:00 AM to 10:00 PM.</p>
-            </div>
-            <div class="faq-item">
-                <h3>Do you offer delivery?</h3>
-                <p>Yes, we offer delivery within a 5-mile radius of our restaurant.</p>
-            </div>
-    </div>
-       
-    `;
 
     contact.innerHTML = `
     <div class="wrapper-col"> 
@@ -151,11 +118,9 @@ export function loadPage() {
     `;
 
     content.appendChild(hero);
-    content.appendChild(about);
     content.appendChild(menu);
-    // content.appendChild(faq);
     content.appendChild(contact);
 
-    console.log('Home');
+    console.log('Menu');
     
 }
